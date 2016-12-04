342bbd66d8bab61e7bcd2c75c9f49d4593c9f3e1
Leandro <leandrolg13.lg@gmail.com>
2016-12-04 19:12:45 -0200

    Variável de ambiente

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'root',
+    host     : process.env.OPENSHIFT_DB_HOST,
+    user     : process.env.OPENSHIFT_DB_USERNAME,
+    password : process.env.OPENSHIFT_DB_PASSWORD,
     database : 'controle_vendas'
 });
 
